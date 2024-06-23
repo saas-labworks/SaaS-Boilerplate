04c9cf99532554b5c08f96297b4961836dc7dd73
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-06-23 16:15:15 -0400

    feature()[]: add auth configuration with prisma adapter

---
+++ lib/auth.ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth"
+import authConfig from "./auth.config"
+import { PrismaClient } from "@prisma/client"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+
+const prisma = new PrismaClient()
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: 'jwt' },
+  ...authConfig
+})
