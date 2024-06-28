edb091d65b2a089951c73b81c22444e912558854
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-06-27 22:44:45 -0400

    feature(ui)[]: add base hero page

---
+++ components/Hero.tsx
@@ -0,0 +1,16 @@
+export function Hero () {
+  return (
+    <section className='hero bg-base-200 min-h-screen'>
+      <div className='hero-content text-center'>
+        <div className='max-w-md'>
+          <h1 className='text-5xl font-bold'>Hello there</h1>
+          <p className='py-6'>
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+            quasi. In deleniti eaque aut repudiandae et a id nisi.
+          </p>
+          <button className='btn btn-primary w-52 rounded-badge'>Start Building</button>
+        </div>
+      </div>
+    </section>
+  )
+}
