a38124e318ac29c7891f01ec8780e1a6800848cf
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-08-18 23:59:49 -0400

    feature()[]: add new page with pricing and faq

---
+++ app/plans/page.tsx
@@ -0,0 +1,11 @@
+import { Faqs } from '@/components/Faqs'
+import { Pricing } from '@/components/Pricing'
+
+export default function PaymentPage() {
+  return (
+    <>
+      <Pricing />
+      <Faqs />
+    </>
+  )
+}
