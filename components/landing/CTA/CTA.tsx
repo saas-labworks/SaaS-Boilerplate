41b71f971b60e999b2185beacabff165c7876ce2
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-08-20 23:30:08 -0400

    fix()[]: change CTA name

--- components/landing/CTA/CTA.tsx
+++ components/landing/CTA/CTA.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button'
 
-export function CTA() {
+export function Cta() {
   return (
     <section className='w-full min-h-screen flex flex-col items-center justify-center'>
       <h3 className='text-3xl text-center font-bold '>
