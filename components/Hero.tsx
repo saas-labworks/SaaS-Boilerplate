e0acb060a59a3fbac1d9fe8bd76323567d7e0410
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-08-25 10:30:49 -0400

    feature()[]: add some testimonials examples with senja

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,17 +1,29 @@
-import { Button } from './ui/button'
-
 // https://shipfa.st/docs/components/hero
-export function Hero () {
+export function Hero() {
   return (
     <section className='w-full min-h-screen flex justify-center items-center'>
-      <div className='max-w-md flex flex-col items-center'>
-        <h1 className='text-5xl font-bold'>Hello there</h1>
-        <p className='py-6 text-center'>
+      <div className='max-w-md flex flex-col gap-3 items-center'>
+        <h1 className='text-7xl font-bold'>Hello there</h1>
+        <p className='text-center'>
           Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-          quasi. In deleniti eaque aut repudiandae et a id nisi.
+          quasi.
         </p>
 
-        <Button>Start Building</Button>
+        <div className='w-full flex justify-center'>
+          <script src='https://widget.senja.io/widget/1bcdaf20-3387-4a19-8f98-914178440bbb/platform.js' type='text/javascript' async />
+          <div
+            className='senja-embed'
+            data-id='1bcdaf20-3387-4a19-8f98-914178440bbb'
+            data-mode='shadow'
+            data-lazyload='false'
+            style={{
+              display: 'block'
+            }}
+          />
+        </div>
+      </div>
+      <div>
+        {/* <Image src='/hero.png' alt='hero' width={500} height={500} /> */}
       </div>
     </section>
   )
---
+++ components/landing/testimonials/TestimonialsGrid.tsx
@@ -0,0 +1,20 @@
+export function TestimonialsGrid() {
+  return (
+    <div className='w-full'>
+      <script
+        src='https://widget.senja.io/widget/e6b7febd-25d6-4d68-af73-b4090a48fd2e/platform.js'
+        type='text/javascript'
+        async
+      />
+      <div
+        className='senja-embed'
+        data-id='e6b7febd-25d6-4d68-af73-b4090a48fd2e'
+        data-mode='shadow'
+        data-lazyload='false'
+        style={{
+          display: 'block'
+        }}
+      />
+    </div>
+  )
+}
