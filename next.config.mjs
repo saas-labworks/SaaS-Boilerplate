181edc1aedc552eff51583739fd2fd9ecda8267e
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-09-01 11:24:10 -0400

    chore()[]: config next image to show google images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        // google images
+        hostname: 'lh3.googleusercontent.com'
+      }
+    ]
+  }
+}
 
 export default nextConfig
