12f16ed8e78221a8697ca73b5928befb5bd45dc8
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-09-07 22:50:46 -0400

    feature()[]: update stripe webhook to manage subscription

--- src/app/api/webhook/stripe/route.ts
+++ src/app/api/webhook/stripe/route.ts
@@ -1,22 +1,20 @@
-import { AppConstants } from '@/src/lib/config'
-import { getUserByCustomerId, getUserByEmail, giveAccess, revokeAccess, updateUser } from '@/src/lib/data-access'
-import { sendCancelledPayment, sendCompletePayment } from '@/src/lib/email'
+import Stripe from 'stripe'
 import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
-import Stripe from 'stripe'
-
-const stripe = new Stripe(AppConstants.StripeSecretKey)
-const webhookSecret = AppConstants.StripeWebhookSecret
+import { AppConstants } from '@/src/lib/config'
+import { getUserById, giveAccess } from '@/src/lib/data-access'
+import { createSubscription, updateSubscription } from '@/src/lib/data-access/subscription'
+import { sendCompletePayment } from '@/src/lib/email'
+import { stripe } from '@/src/lib/stripe'
+import * as fs from 'fs'
 
 export async function POST(req: Request) {
-  console.log('Icomming stripe request [Webhook]')
-
   const body = await req.text()
   const signature = headers().get('stripe-signature')!
   let event: Stripe.Event
 
   try {
-    event = stripe.webhooks.constructEvent(body, signature, webhookSecret)
+    event = stripe.webhooks.constructEvent(body, signature, AppConstants.StripeWebhookSecret)
   } catch (error) {
     const { message } = error as unknown as Error
     console.error(`Webhook signature verification failed. ${message}`)
@@ -25,7 +23,6 @@ export async function POST(req: Request) {
 
   const data = event.data
   const eventType = event.type
-  console.log(`Incomming stripe event: ${eventType}`)
 
   try {
     switch (eventType) {
@@ -33,54 +30,94 @@ export async function POST(req: Request) {
         // First payment is successful and a subscription is created (if mode was set to "subscription" in ButtonCheckout)
         // ✅ Grant access to the product
         const session = await stripe.checkout.sessions.retrieve(
-          (data.object as { id: string }).id,
+          (data.object as Stripe.Checkout.Session).id,
           { expand: ['line_items'] }
         )
 
+        fs.writeFileSync('session-details.json', JSON.stringify(session, null, 2))
+
+        const userId = session.metadata!.userId
         const customerId = session.customer!.toString()
-        const customer = (await stripe.customers.retrieve(customerId)) as Stripe.Customer
-        const priceId = session.line_items?.data[0].price?.id!
+        const priceId = session.line_items!.data[0].price!.id
 
-        console.log(`Searchig the user ${customer.email} in the database.`)
-        const user = await getUserByEmail(customer.email!)
-
-        if (!user) {
-          console.error(`User ${customer.email} not found in database`)
-          throw new Error(`User ${customer.email} not found in database`)
+        if (session.mode === 'payment') {
+          // TODO: prices unique payment
         }
 
-        // assign Customer Id
-        await updateUser(user.id, { customerId })
+        if (session.mode === 'subscription') {
+          const subscription = await stripe.subscriptions.retrieve(
+            session.subscription!.toString()
+          )
 
+          await createSubscription({
+            userId,
+            stripeSubscriptionId: subscription.id,
+            stripeCustomerId: customerId,
+            stripePriceId: priceId,
+            stripeCurrentPeriodStart: new Date(subscription.current_period_start * 1000),
+            stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000)
+          })
+        }
+
+        const user = await getUserById(session.metadata!.userId)
         // Update user data + Grant user access to your product. It's a boolean in the database, but could be a number of credits, etc...
-        await giveAccess(user.id, priceId)
+        await giveAccess(user!.id)
 
-        // TODO: Extra: >>>>> send email to dashboard <<<<
-        await sendCompletePayment({ to: user.email! })
+        // Extra: >>>>> send email to dashboard <<<<
+        await sendCompletePayment({ to: user!.email! })
+
+        // subscription.customer // => customerId
+        // subscription.id // => subscriptionId
+        // subscription.current_period_start // => start of the current period
+        // subscription.current_period_end // => end of the current period
+        // subscription.items // => list of items in the subscription
+
+        // subscription.plan.id // => priceId
+        // subscription.plan.amount_decimal // => price
+        // subscription.plan.interval // => interval (month, year, week, day)
+        // subscription.plan.trial_period_days // => trial period in days
+
+        // subscription.trial_start // => trial start date
+        // subscription.trial_end // => trial end date
+
         break
       }
 
+      case 'customer.subscription.created':
+      case 'customer.subscription.updated':
       case 'customer.subscription.deleted': {
-        // ❌ Revoke access to the product
-        // The customer might have changed the plan (higher or lower plan, cancel soon etc...)
         const subscription = await stripe.subscriptions.retrieve(
-          (data.object as { id: string }).id
+          (data.object as Stripe.Checkout.Session).id
         )
 
-        const customerId = subscription.customer!.toString()
-        const user = await getUserByCustomerId(customerId)
-
-        if (!user) {
-          console.error('No user found')
-          throw new Error('No user found')
-        }
-
-        // Revoke access to your product
-        await revokeAccess(user.id)
-        // TODO: Extra: >>>>> send email to dashboard <<<<
-        await sendCancelledPayment({ to: user.email! })
+        await updateSubscription({
+          stripePriceId: subscription.items.data[0].price!.id,
+          stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
+          stripeSubscriptionId: subscription.id
+        })
         break
       }
+
+      // case 'customer.subscription.deleted': {
+      //   // ❌ Revoke access to the product
+      //   // The customer might have changed the plan (higher or lower plan, cancel soon etc...)
+      //   const subscription = await stripe.subscriptions.retrieve(
+      //     (data.object as Stripe.Checkout.Session).id
+      //   )
+
+      //   const user = await getUserById(subscription.metadata!.userId)
+      //   if (!user) {
+      //     console.error('No user found')
+      //     throw new Error('No user found')
+      //   }
+
+      //   // Revoke access to your product
+      //   await revokeAccess(user.id)
+
+      //   //  Extra: >>>>> send email to dashboard <<<<
+      //   await sendCancelledPayment({ to: user.email! })
+      //   break
+      // }
 
       default:
         // Unhandled event type
